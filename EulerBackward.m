7a5228fefe6d1320ea628c05de3f1923452f6d33
salmamal <salmamedhat637@gmail.com>
2025-03-10 01:40:50 +0200

    EulerBackward.m

--- EulerBackward.m
+++ EulerBackward.m
@@ -5,7 +5,7 @@ y = zeros(1, N+1);
 x(1) = xinit;
 y(1) = yinit;
 for i=1:N
- x(i-1) = x(i)-h;
- y(i-1) = y(i) - h*feval(f,x(i),y(i));
+ x(i+1) = x(i)+h;
+ y(i+1) = y(i) + h*feval(f,x(i+1),y(i+1));
 end
 end
